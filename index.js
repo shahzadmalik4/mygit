aff03ff6eacecbe8970dae52fc1843150b3bc199
shahzad <shahzadmalik732@gmail.com>
2025-01-17 19:47:17 +0500

    myfirst

---
+++ index.js
@@ -0,0 +1,23 @@
+//variable
+
+// var fullname;//declare
+// fullname ="abdul khan <br>";//init
+
+var fullname ="abdul khan <br>";//declare & init
+document.write(fullname);
+document.write(fullname);
+document.write(fullname);
+document.write(fullname);
+document.write(fullname);
+document.write(fullname);
+
+
+document.write("hello world <br>")
+
+
+document.write("big data")
+
+console.log("world!");
+console.log("world jeeeee!");
+// alert("hello this is alert function")
+
